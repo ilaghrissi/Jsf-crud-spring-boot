4bf28b5abaf97f8783c8b3badbb5f2eda0470d02
LAGHRISSI Imad <laghrissi.imad@gmail.com>
2021-11-03 12:18:15 +0100

    Add confirm delete/update dialog with selected element + internationnalization

--- src/main/java/com/tutorial/beans/BookBean.java
+++ src/main/java/com/tutorial/beans/BookBean.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.springframework.stereotype.Component;
 
@@ -23,7 +26,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Component
 @ManagedBean
-@ViewScoped
+@RequestScoped
 public class BookBean {
 
 	private List<Book> books = new ArrayList<Book>();
@@ -41,4 +44,18 @@ public class BookBean {
 		b2.setPrice(30);
 		this.books = Arrays.asList(b1, b2);
 	}
+	
+	public void update() {
+		System.out.println("update element"+this.selected);
+		addMessage("Confirmed", "You have accepted");
+	}
+	
+	public void delete() {
+		System.out.println("delete element"+this.selected);
+	}
+	
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 }
